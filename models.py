53c2f63e6df6ab9f311e94e28d56a3bf9a0669ac
sam <wax654@gmail.com>
2021-03-25 23:29:27 -0700

    Added cupcake model

--- models.py
+++ models.py
@@ -1,1 +1,29 @@
 """Models for Cupcake app."""
+
+"""Database Setup"""
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    """Initiates a connection to the DB"""
+    db.app = app
+    db.init_app(app)
+    db.create_all()
+
+
+class Cupcake(db.Model):
+    __tablename__ = 'cupcakes'
+    
+    DEFAULT_IMAGE_URL = 'https://tinyurl.com/demo-cupcake'
+    
+    id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    flavor = db.Column(db.Text, nullable=False)
+    size = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    image = db.Column(db.Text, server_default=DEFAULT_IMAGE_URL)
+    notes = db.Column(db.Text)
+    available = db.Column(db.Boolean, nullable=False, default=True)
+
+    
